cfd6499016db21b457b57d2ac5d1a34e66f3af64
Shums Kassam <skassam21@gmail.com>
2019-07-04 17:29:03 -0400

    small front-end fix

--- client/src/pages/Ping.js
+++ client/src/pages/Ping.js
@@ -7,7 +7,7 @@ function Ping(props) {
   const [answer, setAnswer] = useState("");
   useEffect(() => {
     props.incrementStep();
-  }, []);
+  });
 
   const submitAnswer = () => {
     console.log("run");
